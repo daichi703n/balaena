ecae76e66ff41838cdf67d0ea2639592168eda18
Daichi Kimura <daichi703n@gmail.com>
2018-05-03 01:48:14 +0900

    Add PHP and Other

--- config/command.sh
+++ config/command.sh
@@ -2,5 +2,5 @@
 echo start
 php -S 0.0.0.0:80 &
 ping -c 3 -W 1 -s 1472 192.168.1.5
-traceroute -n 8.8.8.8
+traceroute 8.8.8.8
 curl localhost
---
+++ config/php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <?php
+            print "<pre>\n";
+            print_r($_SERVER);
+            print "</pre>\n";
+        ?>
+
+     </body>
+</html>
